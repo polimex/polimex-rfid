5ac6341326a84cb23b23dde0498346bf27038254
Ilian Georgiev <zelin11@live.com>
2019-04-24 16:53:58 +0300

    hr_attendance_multi_rfid: Now show in the events if the person is going in or out

--- hr_attendance_multi_rfid/models/hr_rfid_event_user.py
+++ hr_attendance_multi_rfid/models/hr_rfid_event_user.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
-from odoo import models, api
+from odoo import models, api, fields
 
 
 class HrRfidUserEvent(models.Model):
     _inherit = "hr.rfid.event.user"
+
+    in_or_out = fields.Selection(
+        selection=[ ('0', 'In'), ('1', 'Out'), ('2', 'No Info') ],
+        help='Whether the user came in or out',
+        string='User presence',
+        default='2',
+    )
 
     @api.model_create_multi
     @api.returns('self', lambda value: value.id)
@@ -15,8 +22,10 @@ class HrRfidUserEvent(models.Model):
 
             if ev.door_id.attendance is True and ev.event_action == '1':
                 if ev.reader_id.reader_type == '0' and ev.user_id.attendance_state == 'checked_out':
+                    ev.in_or_out = '0'
                     ev.user_id.attendance_action_change_with_date(ev.event_time)
                 elif ev.reader_id.reader_type == '1' and ev.user_id.attendance_state == 'checked_in':
+                    ev.in_or_out = '1'
                     ev.user_id.attendance_action_change_with_date(ev.event_time)
 
         return records
