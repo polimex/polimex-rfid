fdaaabd34533556238ebd075136a1cec59b09a59
Ilian Georgiev <zelin11@live.com>
2019-05-11 13:55:22 +0300

    Event filters and card "activate_on" default to right now

--- hr_rfid/models/hr_rfid_card.py
+++ hr_rfid/models/hr_rfid_card.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, exceptions
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 
 Employee = 1
@@ -62,6 +62,7 @@ class HrRfidCard(models.Model):
         string='Activate on',
         help='Date and time the card will be activated on',
         track_visibility='onchange',
+        default=lambda self: datetime.now(),
     )
 
     deactivate_on = fields.Datetime(
