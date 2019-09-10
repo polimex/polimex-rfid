754e8010b6f80b66ba7add8d1c98fa5561f5b82d
Ilian Georgiev <zelin11@live.com>
2019-09-10 14:11:15 +0300

    Initial commit

---
+++ hr_rfid_vending/controllers/main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import http, fields, exceptions
+from odoo.http import request
+
+from odoo.addons.hr_rfid.controllers.main import WebRfidController
+
+
+class HrRfidVending(WebRfidController):
+    @http.route(['/hr/rfid/event'], type='json', auth='none', method=['POST'], csrf=False)
+    def post_event(self, **post):
+
+        def ret_super():
+            return super(HrRfidVending, self).post_event(post)
+
+        if 'event' in post:
+            ret = parse_event()
+        else:
+            ret = ret_super()
+
+        return ret
+
