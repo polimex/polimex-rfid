44d6ab23b697d963978dcba862585391dabfe187
Ilian Georgiev <zelin11@live.com>
2019-09-16 16:42:41 +0300

    Implementations

--- hr_rfid/models/hr_rfid_webstack.py
+++ hr_rfid/models/hr_rfid_webstack.py
@@ -350,7 +350,44 @@ class HrRfidCtrlIoTableRow(models.TransientModel):
     _name = 'hr.rfid.ctrl.io.table.row'
     _description = 'Controller IO Table row'
 
-    controller_id = fields.Many2one('hr.rfid.ctrl')
+    event_codes = [
+        ('1' , "Duress"),
+        ('2' , "Duress Error"),
+        ('3' , "Reader #1 Card OK"),
+        ('4' , "Reader #1 Card Error"),
+        ('5' , "Reader #1 TS Error"),
+        ('6' , "Reader #1 APB Error"),
+        ('7' , "Reader #2 Card OK"),
+        ('8' , "Reader #2 Card Error"),
+        ('9' , "Reader #2 TS Error"),
+        ('10', "Reader #2 APB Error"),
+        ('11', "Reader #3 Card OK"),
+        ('12', "Reader #3 Card Error"),
+        ('13', "Reader #3 TS Error"),
+        ('14', "Reader #3 APB Error"),
+        ('15', "Reader #4 Card OK"),
+        ('16', "Reader #4 Card Error"),
+        ('17', "Reader #4 TS Error"),
+        ('18', "Reader #4 APB Error"),
+        ('19', "Emergency Input"),
+        ('20', "Arm On Siren"),
+        ('21', "Exit Button 1"),
+        ('22', "Exit Button 2"),
+        ('23', "Exit Button 3"),
+        ('24', "Exit Button 4"),
+        ('25', "Door Overtime"),
+        ('26', "Door Forced Open"),
+        ('27', "On Delay"),
+        ('28', "Off Delay"),
+    ]
+
+    event_number = fields.Selection(
+        selection=event_codes,
+        string='Event Number',
+        help='What the outs are set to when this event occurs',
+        required=True,
+        readonly=True,
+    )
 
     # Range is from 00 to 99
     out8 = fields.Integer(string='Out8', required=True)
@@ -370,9 +407,61 @@ class HrRfidCtrlIoTableWiz(models.TransientModel):
     def _default_ctrl(self):
         return self.env['hr.rfid.ctrl'].browse(self._context.get('active_ids'))
 
-    controller_id = fields.Many2one('hr.rfid.ctrl', default=_default_ctrl, required=True)
+    def _generate_io_table(self):
+        rows_env = self.env['hr.rfid.ctrl.io.table.row']
+        row_len = 8 * 2  # 8 outs, 2 symbols each to display the number
+        ctrl = self._default_ctrl()
 
-    io_row_ids =
+        if len(ctrl.io_table) % row_len != 0:
+            raise exceptions.ValidationError('Controller does now have an input/output table loaded!')
+
+        io_table = ctrl.io_table
+        rows = rows_env
+
+        for i in range(0, len(ctrl.io_table), row_len):
+            creation_dict = { 'event_number': str(int(i / row_len) + 1) }
+            for j in range(8, 0, -1):
+                index = i + ((8 - j) * 2)
+                creation_dict['out' + str(j)] = int(io_table[index:index+2])
+            rows += rows_env.create(creation_dict)
+
+        return rows
+
+    def _default_outs(self):
+        return self._default_ctrl().outputs
+
+    controller_id = fields.Many2one(
+        'hr.rfid.ctrl',
+        default=_default_ctrl,
+        required=True
+    )
+
+    io_row_ids = fields.Many2many(
+        'hr.rfid.ctrl.io.table.row',
+        string='IO Table',
+        default=_generate_io_table,
+    )
+
+    outs = fields.Integer(
+        default=_default_outs,
+    )
+
+    @api.multi
+    def save_table(self):
+        self.ensure_one()
+
+        new_io_table = ''
+
+        for row in self.io_row_ids:
+            outs = [ row.out8, row.out7, row.out6, row.out5, row.out4, row.out3, row.out2, row.out1 ]
+            for out in outs:
+                if out < 0 or out > 99:
+                    raise exceptions.ValidationError(
+                        _('%d is not a valid number for the io table. Valid values range from 0 to 99') % out
+                    )
+                new_io_table += '%02d' % out
+
+        self.controller_id.change_io_table(new_io_table)  # TODO Implement this method
 
 
 class HrRfidController(models.Model):
