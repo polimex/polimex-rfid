00c5e56140e16a9a14da90071c5556e7c7871c7a
Ilian Georgiev <zelin11@live.com>
2019-10-26 13:54:44 +0300

    2 more modules for auto refresh for the event-type models

---
+++ hr_rfid_vending_muk_web_client_refresh/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# noinspection PyStatementEffect
+{
+    'name': 'hr_rfid_vending_muk_web_client_refresh',
+    'version': '0.1',
+    'category': 'Uncategorized',
+    'summary': '',
+    'author': 'Polimex',
+    'license': 'AGPL-3',
+
+    'website': 'https://www.securitybulgaria.com/',
+
+    'depends': [ 'base', 'hr_rfid_vending', 'muk_web_client_refresh' ],
+
+    'data': [ 'data/auto_refresh_data.xml' ],
+
+    'demo': [ ],
+
+    'application': False,
+    'installable': True,
+    'auto_install': True,
+}
