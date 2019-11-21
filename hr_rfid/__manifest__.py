b675c71885389f27dc013c6a8604aa4c6fda7c5f
Ilian Georgiev <zelin11@live.com>
2019-11-21 13:54:35 +0200

    hr_rfid: Fix the system event add card button when logging is disabled

--- hr_rfid/__manifest__.py
+++ hr_rfid/__manifest__.py
@@ -2,7 +2,7 @@
 # noinspection PyStatementEffect
 {
     'name': 'RFID Access Control',
-    'version': '1.10',
+    'version': '1.11',
     'category': 'Human Resources',
     'summary': 'Manage employee access control',
     'author': 'Polimex',
