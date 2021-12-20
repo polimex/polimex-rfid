fb107844c10264c624352b2237cdbfe5c9d252b3
Lubo <lgeorgiev@securitybulgaria.com>
2021-12-20 17:02:02 +0200

    Config settings update & bugfix

--- hr_rfid/models/hr_rfid_settings.py
+++ hr_rfid/models/hr_rfid_settings.py
@@ -7,11 +7,8 @@ class RfidSettings(models.TransientModel):
     event_lifetime = fields.Integer(string='Event life time', default=365,
                                     config_parameter='hr_rfid.event_lifetime',
                                     help='Enter event lifetime. Older events will be deleted')
-    save_webstack_communications = fields.Selection([('True', 'On'),
-                                                    ('False', 'Off'),
-                                                    ],string='Save WebStack communication', default='False',
-                                                    config_parameter='hr_rfid.save_webstack_communications',
-                                                    help='Save WebStack communication')
-
+    module_hr_rfid_vending = fields.Boolean(string="Vending Control")
+    module_rfid_pms_base = fields.Boolean(string="PMS Base functionality")
+    module_hr_rfid_andromeda_import = fields.Boolean(string="Andromeda Import")
 
 
