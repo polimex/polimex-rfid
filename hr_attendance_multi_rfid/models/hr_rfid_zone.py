870fbedecd438f1249c2a8ecfc04578bdf3b1cdf
Lubo <lgeorgiev@securitybulgaria.com>
2022-05-30 13:18:51 +0300

    Attendance zone fix

--- hr_attendance_multi_rfid/models/hr_rfid_zone.py
+++ hr_attendance_multi_rfid/models/hr_rfid_zone.py
@@ -47,13 +47,10 @@ class HrRfidZone(models.Model):
             if zone.attendance is False:
                 continue
 
-            if person in zone.employee_ids and zone.overwrite_check_in:
-                check = self.env['hr.attendance'].search([('employee_id', '=', person.id)], limit=1)
-                if check.check_out:
-                    continue
+            if person.attendance_state == 'checked_in' and zone.overwrite_check_in:
                 event.in_or_out = 'in'
-                check.check_in = event.event_time
-            elif person not in zone.employee_ids and person.attendance_state == 'checked_out':
+                person.check_in = event.event_time
+            elif person.attendance_state == 'checked_out':
                 event.in_or_out = 'in'
                 person.attendance_action_change_with_date(event.event_time, zone.id)
         return super(HrRfidZone, self).person_entered(person, event)
@@ -63,14 +60,14 @@ class HrRfidZone(models.Model):
             return super(HrRfidZone, self).person_left(person, event)
 
         for zone in self.filtered(lambda z: z.attendance and event):
-            if person not in zone.employee_ids and zone.overwrite_check_out:
+            if person.attendance_state == 'checked_out' and zone.overwrite_check_out:
                 check = self.env['hr.attendance'].search([('employee_id', '=', person.id)], limit=1)
                 if event:
                     event.in_or_out = 'out'
                     check.check_out = event.event_time
                 else:
                     check.check_out = fields.datetime.now()
-            elif person in zone.employee_ids and person.attendance_state == 'checked_in':
+            elif person.attendance_state == 'checked_in':
                 if event:
                     event.in_or_out = 'out'
                     person.attendance_action_change_with_date(event.event_time, zone.id)
