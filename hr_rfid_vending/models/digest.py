eb7897abe37adf6ddf01d4a53abceaf8c4c4ed90
Lyubomir Georgiev <lgeorgiev@securitybulgaria.com>
2023-05-26 11:41:08 +0300

    fix kpi field type sales count

--- hr_rfid_vending/models/digest.py
+++ hr_rfid_vending/models/digest.py
@@ -14,7 +14,7 @@ class Digest(models.Model):
     kpi_hr_rfid_vending_sale_count = fields.Boolean('Sales count')
     kpi_hr_rfid_vending_refill_value = fields.Monetary(compute='_compute_kpi_hr_rfid_vending_values')
     kpi_hr_rfid_vending_sale_value = fields.Monetary(compute='_compute_kpi_hr_rfid_vending_values')
-    kpi_hr_rfid_vending_sale_count_value = fields.Monetary(compute='_compute_kpi_hr_rfid_vending_values')
+    kpi_hr_rfid_vending_sale_count_value = fields.Integer(compute='_compute_kpi_hr_rfid_vending_values')
 
     def _compute_kpi_hr_rfid_vending_values(self):
         if not self.env.user.has_group('hr_rfid_vending.group_operator'):
