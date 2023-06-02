4d49cdab1b6ddcf3448b12d2222de5a0c578b251
Lyubomir Georgiev <lgeorgiev@securitybulgaria.com>
2023-06-02 13:56:11 +0300

    hr_attendance.py add create method

--- hr_attendance_late/models/hr_attendance.py
+++ hr_attendance_late/models/hr_attendance.py
@@ -4,6 +4,12 @@ from odoo import api, fields, models
 class HrAttendance(models.Model):
     _inherit = 'hr.attendance'
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        res.mapped('employee_id').update_extra_attendance_data(self.check_in.date(),overwrite_existing=True)
+        return res
+
     def write(self, vals):
         super(HrAttendance, self).write(vals)
         self.mapped('employee_id').update_extra_attendance_data(self.check_in.date(),overwrite_existing=True)
