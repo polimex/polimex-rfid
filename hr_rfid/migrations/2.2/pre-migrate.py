e52b4f66ccd469cb609d5d2bebb8cf2cfa4b8bfe
Lyubomir Georgiev <lgeorgiev@securitybulgaria.com>
2023-12-12 11:30:51 +0200

    Fix migration

--- hr_rfid/migrations/2.2/pre-migrate.py
+++ hr_rfid/migrations/2.2/pre-migrate.py
@@ -1,7 +1,7 @@
 from odoo.tools import drop_view_if_exists
 
 def migrate(cr, version):
-    drop_view_if_exists(cr, 'hr_rfid_access_group_contact_rel_check_duplicates')
+    # drop_view_if_exists(cr, 'hr_rfid_access_group_contact_rel_check_duplicates')
     cr.execute('ALTER TABLE hr_rfid_access_group_contact_rel DROP CONSTRAINT IF EXISTS hr_rfid_access_group_contact_rel_check_duplicates')
-    drop_view_if_exists(cr, 'hr_rfid_access_group_employee_rel_check_duplicates')
+    # drop_view_if_exists(cr, 'hr_rfid_access_group_employee_rel_check_duplicates')
     cr.execute('ALTER TABLE hr_rfid_access_group_employee_rel DROP CONSTRAINT IF EXISTS hr_rfid_access_group_employee_rel_check_duplicates')
