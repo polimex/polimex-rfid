ac391b27501a43e82a255535447a10d8655dbf6a
Lyubomir Georgiev <lgeorgiev@securitybulgaria.com>
2023-12-12 13:20:13 +0200

    Add Replacement wizard controller selection

--- hr_rfid/models/hr_rfid_webstack_replace_wiz.py
+++ hr_rfid/models/hr_rfid_webstack_replace_wiz.py
@@ -20,10 +20,8 @@ class HrRfidWebstackReplaceWiz(models.TransientModel):
         comodel_name='hr.rfid.webstack',
         context={'active_test': False},
     )
-    source_controller_ids = fields.One2many(
-        'hr.rfid.ctrl',
-        'webstack_id',
-        related='source_webstack_id.controllers'
+    source_controller_ids = fields.Many2many(
+        comodel_name='hr.rfid.ctrl',
     )
     destination_controller_ids = fields.One2many(
         'hr.rfid.ctrl',
@@ -31,7 +29,12 @@ class HrRfidWebstackReplaceWiz(models.TransientModel):
         related='destination_webstack_id.controllers'
     )
     replace_existing = fields.Boolean(
-        help="Replace existing controllers in destination module"
+        help="Replace existing controllers in destination module. \n"
+             "This will remove the existing controller in destination module"
+    )
+    destination_active_state = fields.Boolean(
+        help="The Active state of the destination module.",
+        default=True,
     )
 
     @api.onchange('destination_webstack_id')
@@ -44,8 +47,10 @@ class HrRfidWebstackReplaceWiz(models.TransientModel):
     @api.onchange('source_webstack_id')
     def _onchange_source_webstack_id(self):
         if self.source_webstack_id:
+            self.source_controller_ids = [(6, 0, self.source_webstack_id.controllers.ids)]
             return {'domain': {'destination_webstack_id': [('id', '!=', self.source_webstack_id.id)]}}
         else:
+            self.source_controller_ids = [(6, 0, [])]
             return {'domain': {'destination_webstack_id': []}}
 
     def confirm_transfer(self):
@@ -55,9 +60,9 @@ class HrRfidWebstackReplaceWiz(models.TransientModel):
             raise ValidationError(_('Use different modules for this operation!'))
         if self.replace_existing:
             self.destination_webstack_id.controllers.unlink()
-        self.source_webstack_id.controllers.webstack_id = self.destination_webstack_id.id
+        self.source_controller_ids.webstack_id = self.destination_webstack_id.id
         self.source_webstack_id.active = False
-        self.destination_webstack_id.active = True
+        self.destination_webstack_id.active = self.destination_active_state
         base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
 
         link = f"{base_url}/web#id={self.destination_webstack_id.id}&model=hr.rfid.webstack&view_type=form"
@@ -67,5 +72,6 @@ class HrRfidWebstackReplaceWiz(models.TransientModel):
         link = f"{base_url}/web#id={self.source_webstack_id.id}&model=hr.rfid.webstack&view_type=form"
         message = _("Controllers moved from <a href='%s'>Module %s</a>", link, self.source_webstack_id.display_name)
         self.destination_webstack_id.message_post(body=message, message_type='comment')
-
+        message = _("Controller moved from <a href='%s'>Module %s</a>", link, self.source_webstack_id.display_name)
+        self.source_controller_ids.message_post(body=message, message_type='comment')
         pass
