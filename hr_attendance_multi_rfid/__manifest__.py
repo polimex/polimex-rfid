81465af9a1cd800ece9a0ba267e17ba727194a37
Lyubomir Georgiev <lgeorgiev@securitybulgaria.com>
2024-01-15 12:10:45 +0200

    add rights for attendance manager to resource.calendar model and make it multi-company

--- hr_attendance_multi_rfid/__manifest__.py
+++ hr_attendance_multi_rfid/__manifest__.py
@@ -14,10 +14,11 @@
 
     'website': 'polimex.co',
 
-    'depends': ['hr_rfid', 'hr_attendance', 'hr_attendance_reason', 'hr_attendance_autoclose'],
+    'depends': ['resource', 'hr_rfid', 'hr_attendance', 'hr_attendance_reason', 'hr_attendance_autoclose'],
 
     'data': [
         'security/ir.model.access.csv',
+        'security/resource_calendar_multi_company.xml',
         'views/hr_attendance.xml',
         'views/hr_rfid_webstack_views.xml',
         'wizards/hr_recalc_attendance_wizard.xml',
