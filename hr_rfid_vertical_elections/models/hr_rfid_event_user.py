d0b734b015bca540367c1e3d6ebd7f2941815992
Lyubomir Georgiev <lgeorgiev@securitybulgaria.com>
2024-10-09 13:27:44 +0300

    customer request fixes

--- hr_rfid_vertical_elections/models/hr_rfid_event_user.py
+++ hr_rfid_vertical_elections/models/hr_rfid_event_user.py
@@ -1,5 +1,7 @@
 from odoo import fields, models, api
 
+import logging
+_logger = logging.getLogger(__name__)
 
 class HrRfidUserEvent(models.Model):
     _inherit = 'hr.rfid.event.user'
@@ -8,10 +10,14 @@ class HrRfidUserEvent(models.Model):
     def create(self, vals_list):
         res = super(HrRfidUserEvent, self).create(vals_list)
         for res in self.filtered(lambda res: res.event_action == '1'):
+            _logger.info('Voting event detected')
             open_vote_session_ids = self.env['voting.session'].search([('state', '=', 'open')])
+            _logger.info('Open voting sessions: %s', open_vote_session_ids)
             open_vote_session_for_this_door = open_vote_session_ids.filtered(lambda session: res.door_id in session.participant_group_id.terminal_ids)
+            _logger.info('Open voting sessions for this door: %s', open_vote_session_for_this_door)
             if open_vote_session_for_this_door and res.contact_id in open_vote_session_for_this_door.participant_group_id.participant_ids:
                 workcode_id = res.workcode_id
+                _logger.info('Workcode: %s', workcode_id)
                 if res.event_time >= open_vote_session_for_this_door[0].start_date and workcode_id.user_action in ['yes', 'no', 'abstain']:
                     vote = self.env['voting.vote'].create({
                     'voting_item_id': open_vote_session_for_this_door[0].item_ids.ids[0],
@@ -20,6 +26,7 @@ class HrRfidUserEvent(models.Model):
                     'vote': workcode_id.user_action,
                     'vote_event_id': res.id,
                 })
+                    _logger.info('Vote created: %s', vote)
         return res
 
     def re_vote_event(self):
