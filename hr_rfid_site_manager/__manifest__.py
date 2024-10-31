835e116d1fa63f40363eca8bebe0f52fce55053d
Lyubomir Georgiev <lgeorgiev@securitybulgaria.com>
2024-10-31 20:48:32 +0200

    imp alarm line group

--- hr_rfid_site_manager/__manifest__.py
+++ hr_rfid_site_manager/__manifest__.py
@@ -27,6 +27,7 @@
         'security/hr_rfid_multi_company.xml',
         'security/ir.model.access.csv',
         'views/hr_rfid_access_group.xml',
+        'views/hr_rfid_ctrl_alarm_line.xml',
         'views/hr_rfid_door.xml',
         'views/hr_rfid_ctrl.xml',
         'views/hr_rfid_webstack.xml',
--- hr_rfid_site_manager/models/__init__.py
+++ hr_rfid_site_manager/models/__init__.py
@@ -6,3 +6,4 @@ from . import hr_rfid_access_group
 from . import hr_rfid_webstack
 from . import hr_rfid_ctrl
 from . import hr_rfid_event_user
+from . import hr_rfid_ctrl_alarm_group
