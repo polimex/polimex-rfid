566ca5221e88c22d978307bed730e840f9291f2d
Lyubomir Georgiev <lgeorgiev@securitybulgaria.com>
2025-06-04 10:40:48 +0300

    Add RFID label printer settings to configuration

---
+++ rfid_service_zpl_labels/models/res_config_settings.py
@@ -0,0 +1,20 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+    
+    # Label printer settings - related to company fields
+    rfid_label_printer_ip = fields.Char(
+        string='Label Printer IP',
+        related='company_id.rfid_label_printer_ip',
+        readonly=False,
+        help='IP address of the Zebra label printer for wristband printing'
+    )
+    
+    rfid_label_printer_port = fields.Integer(
+        string='Label Printer Port', 
+        related='company_id.rfid_label_printer_port',
+        readonly=False,
+        help='TCP port for the Zebra label printer connection (default: 9100)'
+    )
